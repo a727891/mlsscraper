3e190243422518c688c8e4b38c8199893f928aed
Soeed <a727891@gmail.com>
2015-05-02 13:46:56 -0500

    reenabled mongoose

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 var express = require('express');
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var request = require('request');
 var cheerio = require('cheerio');
 var minify = require('html-minifier').minify;
